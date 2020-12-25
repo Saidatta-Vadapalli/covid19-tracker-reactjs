be5e992325b0ac5a649df0351f494534b6062e55
Saidatta.Vadapalli <hello@saidatta.dev>
2020-12-25 06:19:34 +0530

    made changes to the api.js file, corrected the fetchedData var

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ class App extends React.Component {
   };
 
   async componentDidMount() {
-    const fetchData = await fetchData();
+    const fetchedData = await fetchData();
 
-    this.setState({ data: fetchData });
+    this.setState({ data: fetchedData });
   }
   render() {
     const { data } = this.state;
